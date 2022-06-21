a91890b1fcbbbab7dcddf3309c59ceaee7845c22
Lucas Brito <lucasm_brito@hotmail.com>
2022-06-21 19:22:18 -0300

    All Done

--- dist/script.js
+++ dist/script.js
@@ -1,6 +1,20 @@
 "use strict";
 (function () {
     const $ = (querySelector) => document.querySelector(querySelector);
+    function handleTimeSpent(miliseconds) {
+        console.log(miliseconds);
+        const secondsToMiliseconds = 1000;
+        const minuteToMiliseconds = 60 * 1000;
+        let seconds = 0;
+        const minute = Math.ceil(miliseconds / minuteToMiliseconds);
+        if (minute == 0) {
+            seconds = Math.ceil(miliseconds / secondsToMiliseconds);
+        }
+        else {
+            seconds = Math.ceil((miliseconds % minute) / secondsToMiliseconds);
+        }
+        return `${minute}m e ${seconds}s:`;
+    }
     const parking = {
         read() {
             const vehicles = localStorage.getItem("park")
@@ -13,9 +27,14 @@
             row.innerHTML = `
         <td>${vehicle.name}</td>
         <td>${vehicle.license}</td>
-        <td>${vehicle.date}</td>
+        <td>${vehicle.entryDate}</td>
         <td><button class="delete" data-placa="${vehicle.license}">X</button></td>
         `;
+            row.querySelector(".delete")?.addEventListener("click", (e) => {
+                const target = e.target;
+                console.log(String(target.getAttribute("data-placa")));
+                parking.delete(String(target.getAttribute("data-placa")));
+            });
             $("#park")?.appendChild(row);
             if (needSave) {
                 let vehicles = parking.read();
@@ -23,7 +42,23 @@
                 parking.save(vehicles);
             }
         },
-        delete() { },
+        delete(license) {
+            const vehicles = parking.read();
+            const vehicleExists = vehicles.find((vehicle) => vehicle.license === license);
+            if (vehicleExists) {
+                const { name, date } = vehicleExists;
+                const totalTimeSpent = new Date().getTime() - new Date(date).getTime();
+                const time = handleTimeSpent(totalTimeSpent);
+                if (confirm(`O Veiculo ${name} permaneu por ${time}. Deseja encerrar ?`)) {
+                    const updatedVehicles = vehicles.filter((vehicle) => vehicle.license !== license);
+                    parking.save(updatedVehicles);
+                    parking.render();
+                }
+                else {
+                    return;
+                }
+            }
+        },
         save(vehicle) {
             localStorage.setItem("park", JSON.stringify(vehicle));
         },
@@ -48,7 +83,8 @@
         parking.add({
             name,
             license,
-            date: new Date().toLocaleDateString("pt-BR"),
+            entryDate: `${new Date().toLocaleString("pt-BR")}`,
+            date: new Date(),
         }, true);
     });
 })();
--- src/script.ts
+++ src/script.ts
@@ -1,12 +1,29 @@
 interface Vehicle {
   name: string;
   license: string;
-  date: string;
+  entryDate: string;
+  date: Date;
 }
 
 (function () {
   const $ = (querySelector: string): HTMLInputElement | null =>
     document.querySelector(querySelector);
+
+  function handleTimeSpent(miliseconds: number): string {
+    console.log(miliseconds);
+    const secondsToMiliseconds = 1000;
+    const minuteToMiliseconds = 60 * 1000;
+    let seconds = 0;
+
+    const minute = Math.ceil(miliseconds / minuteToMiliseconds);
+    if (minute == 0) {
+      seconds = Math.ceil(miliseconds / secondsToMiliseconds);
+    } else {
+      seconds = Math.ceil((miliseconds % minute) / secondsToMiliseconds);
+    }
+
+    return `${minute}m e ${seconds}s:`;
+  }
 
   const parking = {
     read(): Vehicle[] {
@@ -20,9 +37,16 @@ interface Vehicle {
       row.innerHTML = `
         <td>${vehicle.name}</td>
         <td>${vehicle.license}</td>
-        <td>${vehicle.date}</td>
+        <td>${vehicle.entryDate}</td>
         <td><button class="delete" data-placa="${vehicle.license}">X</button></td>
         `;
+
+      row.querySelector(".delete")?.addEventListener("click", (e) => {
+        const target = e.target as HTMLElement;
+        console.log(String(target.getAttribute("data-placa")));
+
+        parking.delete(String(target.getAttribute("data-placa")));
+      });
 
       $("#park")?.appendChild(row);
       if (needSave) {
@@ -31,7 +55,32 @@ interface Vehicle {
         parking.save(vehicles);
       }
     },
-    delete() {},
+    delete(license: string) {
+      const vehicles = parking.read();
+      const vehicleExists = vehicles.find(
+        (vehicle) => vehicle.license === license
+      );
+
+      if (vehicleExists) {
+        const { name, date } = vehicleExists;
+
+        const totalTimeSpent = new Date().getTime() - new Date(date).getTime();
+
+        const time = handleTimeSpent(totalTimeSpent);
+
+        if (
+          confirm(`O Veiculo ${name} permaneu por ${time}. Deseja encerrar ?`)
+        ) {
+          const updatedVehicles = vehicles.filter(
+            (vehicle) => vehicle.license !== license
+          );
+          parking.save(updatedVehicles);
+          parking.render();
+        } else {
+          return;
+        }
+      }
+    },
     save(vehicle: Vehicle[]) {
       localStorage.setItem("park", JSON.stringify(vehicle));
     },
@@ -59,7 +108,8 @@ interface Vehicle {
       {
         name,
         license,
-        date: new Date().toLocaleDateString("pt-BR"),
+        entryDate: `${new Date().toLocaleString("pt-BR")}`,
+        date: new Date(),
       },
       true
     );
